3b656d3f6abc14343bfcaadaf885cec9c561c618
Stepan0202 <stepan020293@gmail.com>
2023-10-19 00:30:44 +0300

    Personal data updating

--- src/components/pages/UpdateDataPage/UpdateDataPage.jsx
+++ src/components/pages/UpdateDataPage/UpdateDataPage.jsx
@@ -1,32 +1,82 @@
 import './updateDataStyle.scss'
+import { set, ref, get, child, onValue} from 'firebase/database';
+import { db } from '../../../assets/js/firebase/firebase';
+import { useContext, useEffect, useState } from 'react';
+import AuthContext from '../../../assets/js/authentication/auth-context';
+import { useNavigate } from 'react-router';
+
+
+
 export default function UpdateDataPage(){
-    function processSubmit(){
+    const ctx = useContext(AuthContext);
+    const navigateTo = useNavigate();
+    const [age, setAge] = useState("")
+    const [height,setHeight] = useState("") 
+    const [waist,setWaist] = useState("") 
+    const [weight, setWeight] = useState("")
+    function processSubmit(e){
+        e.preventDefault();
+        const name = e.target.name.value;    
+        const age = e.target.age.value;    
+        const height = e.target.height.value;    
+        const weight = e.target.weight.value;    
+        const waist = e.target.waist.value;
+        set(ref(db, 'users/' + ctx.uid + "/age"), age)
+          .then(() => {console.log("Age updated")})
+        set(ref(db, 'users/' + ctx.uid + "/height"), height)
+          .then(() => {console.log("height updated")})
+        set(ref(db, 'users/' + ctx.uid + "/initialWeight"), weight)
+          .then(() => {console.log("weight updated")})
+        set(ref(db, 'users/' + ctx.uid + "/initialWaistSize"), waist)
+          .then(() => {console.log("waist updated")})
 
     };
-
+    function setFormValues(){
+        const dbRef = ref(db);
+        get(child(dbRef, `users/${ctx.uid}`)).then((snapshot) => {
+          if (snapshot.exists()) {
+                const dataFromDB = snapshot.val();
+                console.log(dataFromDB)
+                setAge(dataFromDB.age ? dataFromDB.age : "not set")
+                setWeight(dataFromDB.initialWeight ? dataFromDB.initialWeight : "not set")
+                setHeight(dataFromDB.height ? dataFromDB.height : "not set")
+                setWaist(dataFromDB.initialWaistSize ? dataFromDB.initialWaistSize : "not set")
+          } else {
+            console.log("No data available");
+          }
+        }).catch((error) => {
+          console.error(error);
+        });
+       
+    }
+    useEffect(() => {setFormValues()}, [])
+    
     return(
-        <form onSubmit={processSubmit}>
-            <label>
-                Your name
-                <input type="text" name="name" id="name" />
-            </label>
-            <label>
-                Your age
-                <input type="number" name="age" id="age" />
-            </label>
-            <label>
-                Your height
-                <input type="number" name="height" id="height" /> cm
-            </label>
-            <label>
-                Your weight
-                <input type="number" name="weight" id="weight" /> kg
-            </label>
-            <label>
-                Your waist size
-                <input type="number" name="waist" id="waist" /> cm
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
+        <>
+            <form id="updateUserDataForm" onSubmit={processSubmit}>
+                <label>
+                    Your name
+                    <input type="text" name="name" id="name"  onChange={()=> {}}/>
+                </label>
+                <label>
+                    Your age
+                    <input type="number" name="age" id="age" value = {age} onChange={(e)=> {setAge(e.target.value)}}/>
+                </label>
+                <label>
+                    Your height
+                    <input type="number" name="height" id="height" value={height} onChange={(e)=> {setHeight(e.target.value)}}/> cm
+                </label>
+                <label>
+                    Your weight
+                    <input type="number" name="weight" id="weight" value={weight} onChange={(e)=> {setWeight(e.target.value)}}/> kg
+                </label>
+                <label>
+                    Your waist size
+                    <input type="number" name="waist" id="waist" value={waist} onChange={(e)=> {setWaist(e.target.value)}}/> cm
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            
+        </>
     )
 }
