98b3fb8156c610dc512b1fd63aeb7136e4683b53
nikolay7424 <nikolay7424@gmail.com>
2023-10-20 16:20:54 +0300

    calculator done

--- src/assets/js/authentication/createUserInfo.js
+++ src/assets/js/authentication/createUserInfo.js
@@ -1,15 +1,25 @@
-export default function createUserInfo(){
-    const date = new Date().getTime();
+export default function createUserInfo() {
+    const date = new Date().toISOString().slice(0, 10)
     const userInfo = {
         age: "not set",
-        height: "height not set yet", 
+        height: "height not set yet",
+        gender: "not set",
+        bodyFat: "not set",
+        activity: "not set",
         initialWeight: "not set",
         initialWaistSize: "not set",
-        calendar: {}
+        weight: "not set",
+        calendar: {},
     }
     userInfo.calendar[date] = {
-        weight: "not set",
+        weight: 0,
         waistSize: "not set",
+        carbs: 0,
+        proteins: 0,
+        fats: 0,
+        fiber: 0,
+        consumedCalories: 0,
+        burnedCalories: 0,
     }
-    return userInfo;
+    return userInfo
 }
